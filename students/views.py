3bcce871c0cab79771a0c1d7f0f05533ac0c8d0f
isiahveneracion <156878835+isiahveneracion@users.noreply.github.com>
2024-05-14 02:04:52 +0800

    pa check ko boss

--- students/views.py
+++ students/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .forms import StudentForm
 from .models import Student
 
+'''
 def add_student(request):
     if request.method == "POST":
         form = StudentForm(request.POST)
@@ -17,6 +18,27 @@ def add_student(request):
             )
             student.save()
             return HttpResponse(f"Thank you, {first_name}")
+    else:
+        form = StudentForm()
+    
+    context = {"form": form}
+    return render(request, "student_form.html", context)
+'''
+
+def add_student(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            student = Student(
+                first_name=form.cleaned_data["first_name"],
+                last_name=form.cleaned_data["last_name"],
+                course=form.cleaned_data["course"],
+                gender=form.cleaned_data["gender"],
+                age=form.cleaned_data["age"],
+            )
+            student.save()
+            return redirect('student_table')
     else:
         form = StudentForm()
     
